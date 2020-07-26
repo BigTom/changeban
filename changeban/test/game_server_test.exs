4b863cee825ce65fc711378dbe5bb153159fa096
BigTom <BigTom@users.noreply.github.com>
2020-07-26 07:41:20 +0100

    Update to game state transitions

---
+++ changeban/test/game_server_test.exs
@@ -0,0 +1,23 @@
+defmodule ChangebanGameServerTest do
+  use ExUnit.Case, async: true
+  # doctest Changeban.GameServer
+
+  # setup do
+  #   server = start_supervised(child_spec = %{
+  #     id: GameServer,
+  #     start: {GameServer, :start_link, ["111111"]},
+  #     restart: :transient
+  #   })
+  # end
+
+  # test "initial state", %{game_server: game_server} do
+
+  # end
+
+  # test "New game test number of items" do
+  #   game = Game.new()
+  #   assert Enum.count(game.items) == 16
+  # end
+
+
+end
