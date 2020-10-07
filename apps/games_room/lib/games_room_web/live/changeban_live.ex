860d59184049b45b40f1b34fe954d13debb0c798
BigTom <BigTom@users.noreply.github.com>
2020-10-07 22:56:55 +0100

    Adding documentation Changed instructions on advice from mike

--- apps/games_room/lib/games_room_web/live/changeban_live.ex
+++ apps/games_room/lib/games_room_web/live/changeban_live.ex
@@ -264,55 +264,105 @@ defmodule GamesRoomWeb.ChangebanLive do
 
   def render_join_view(assigns) do
     ~L"""
-        <div class="absolute z-40 flex flex-col items-center justify-center
-                    w-full h-screen
-                    font-sans">
-          <div class="bg-gray-200 rounded shadow p-8 m-4 max-w-md max-h-full">
-            <p class="text-left  font-bold">Do you want to join one a game?</p>
-            <p class="text-left pb-4">Enter your 6 character game name, your one or two character initials and click "Join Game"</p>
-            <form phx-submit="join_game">
-              <div class="grid grid-cols-3 grid-rows-2 gap-2 font-bold">
-                <label class="col-start-1 row-start-1 text-gray-700 text-sm font-bold" for="game_name">Game Name:</label>
-                <input class="col-start-1 row-start-row-2 shadow appearance-none border rounded text-gray-700 uppercase
-                              placeholder-gray-300 focus:outline-none focus:shadow-outline"
-                        placeholder="XXXXXX" name="game_name" id="game_name" type="text" maxlength="6"
-                        <%= if ! is_nil(@game_name) do %> value="<%= @game_name %>" <% end %> >
-                <label class="col-start-2 row-start-1 text-gray-700 text-sm font-bold" for="initials placeholder="XX">Initials:</label>
-                <input class="col-start-2 row-start-2 shadow appearance-none border rounded focus:outline-none focus:shadow-outline placeholder-gray-300"
-                        name="initials" id="initials" type="text" maxlength="2" placeholder="XX">
-                <button type="submit" class="col-start-3 row-start-2  bg-green-300 hover:bg-green-700 text-white font-bold
-                                             rounded focus:outline-none focus:shadow-outline" >
-                  Join Game
-                </button>
-              </div>
-            </form>
-            <p class="text-left pt-2 font-bold">Or start a new one?</p>
-            <p class="text-left pb-4">Enter your one or two character initials, select your Work in Progress (WIP) limit type and click "New Game"</p>
-            <form phx-submit="new_game">
-              <div class="grid grid-cols-3 grid-rows-3 gap-2">
-                <label class="col-start-2 row-start-1 text-gray-700 text-sm font-bold" for="initials">Initials:</label>
-                <input class="col-start-2 row-start-2 shadow appearance-none border rounded focus:outline-nonefocus:shadow-outline placeholder-gray-300"
-                      name="initials" id="initials" type="text" maxlength="2" placeholder="XX">
-                <button type="submit" class="col-start-3 row-start-2 bg-green-300 hover:bg-green-700 text-white font-bold
-                                             rounded focus:outline-none focus:shadow-outline" >
-                  New Game
-                </button>
-                <div class="col-start-1 row-start-3">
-                  <input type="radio" id="none" name="wip" value="none" checked>
-                  <label for="none">None</label>
-                </div>
-                  <div class="col-start-2 row-start-3">
-                  <input type="radio" id="std" name="wip" value="std">
-                  <label for="std">Column</label>
-                </div>
-                <div class="col-start-3 row-start-3">
-                  <input type="radio" id="con" name="wip" value="con">
-                  <label for="con">Consolidated</label>
-                </div>
-              </div>
-            </form>
+      <div class="flex h-screen">
+        <div class="w-2/3 flex flex-1 overflow-y-scroll">
+          <div>
+            <h1 class="py-4 text-xl font-black">Instructions</h1>
+            <p class="pb-2">Changeban is a Lean Startup-flavoured Kanban simulation game.</p>
+            <p class="pb-2">It was created by Mike Burrows, the founder of
+              <a class="text-blue-500" href="https://www.agendashift.com" target="_blank">Agendashift</a>,
+              as an in-person workshop game.  To understand the objectives and outcomes of playing the
+              game have a look at the
+              <a class="text-blue-500" href="https://www.agendashift.com/resources/changeban" target="_blank">Changeban</a>
+              page, on the Agendashift site.</p>
+
+            <p class="pb-2">In this online version you will see what type of turn you have and what you can do.  The game will
+              not allow you to make an invalid move.  Simply look at the bold tickets and pick one.  Clicking
+              on it will make the move.</p>
+            <p class="pb-2">When played in person randomness is introduced for play by each player drawing a card,
+              which could be red or black. Here the game will deal your colour for you.</p>
+            <h2 class="py-2 text-lg">For a RED Card</h2>
+              <li>EITHER advance one of your unblocked items one column rightwards</li>
+              <li>OR unblock one of your blocked items by crossing out its ‘B’ mark</li>
+              <li>OR start a new item if any remain - move it to the first in-progress column</li>
+              <li>If and only if you can’t make one of these moves for yourself, help someone! Advance
+                  or unblock another player’s item Whenever you accept an item, reject another, chosen
+                  by the whole team</li>
+            <h2 class="py-2 text-lg">For a BLACK Card</h2>
+            <p>After your daily standup meeting:</p>
+            <li>BOTH block one of your currently unblocked items if you have any</li>
+            <li>AND start a new item if any remain – even if you had nothing to block</li>
+            <li>If and only if you can’t <i>start a new item</i>, help someone! Advance or unblock another
+                player’s item.</li>
+            <h2 class="py-2 text-lg">Rejecting Items</h2>
+            <p>Whether you are playing a red or black turn whenever you accept an item, reject another,
+              chosen by the whole team</p>
+            <h2 class="py-2 text-lg">Scoring</h2>
+            <p>Changeban simulates the idea that many of our ideas will be rejected as we find out about them.
+               There are two colours of items representing different kinds of work.  To maimise the team's score
+               There should be a balance of completeion and rejection and a balance of types of work.</p>
+            <p>1 point for each Accepted item, up to a maximum of 4 per colour – 8 points available</p>
+            <p>1 point for each colour represented in each subcolumn of Rejected – 8 points available</p>
+            <p>A bonus point for each subcolumn of Rejected that contains exactly 2 items, 1 of each colour –
+               4 points available</p>
+            <p>The maximum score is 20</p>
+            <h2 class="py-2 text-lg">Playing Multiple Games</h2>
+            <p>You cannot restart a game but you can play as many times as you like, simply start a new game.</p>
+            <h2 class="py-2 text-lg">Limiting WIP</h2>
+            <p>The game can be played without WIP limits, with a limit for each column or a consolidated
+              limit across all the in-progress columns.  Simple check the appropriate box when creating a game.</p>
+            <p>After you have started a game the six character game name is displayed in the bottom left
+              corner.  Pass this on to the players for them to join.</p>
+            <p>Each game can hold a maximum of 5 players and you cannot join after it has begun.
+               If you try to join a full, or running game you will be given the opportunity to watch it play."</p>
           </div>
         </div>
+        <div class="w-1/3 rounded shadow b-1 p-4 m-4">
+          <p class="text-left  font-bold">Do you want to join one a game?</p>
+          <p class="text-left pb-4">Enter your 6 character game name, your one or two character initials and click "Join Game"</p>
+          <form phx-submit="join_game">
+            <div class="grid grid-cols-3 grid-rows-2 gap-2 font-bold">
+              <label class="col-start-1 row-start-1 text-gray-700 text-sm font-bold" for="game_name">Game Name:</label>
+              <input class="col-start-1 row-start-row-2 shadow appearance-none border rounded text-gray-700 uppercase
+                            placeholder-gray-300 focus:outline-none focus:shadow-outline"
+                      placeholder="XXXXXX" name="game_name" id="game_name" type="text" maxlength="6"
+                      <%= if ! is_nil(@game_name) do %> value="<%= @game_name %>" <% end %> >
+              <label class="col-start-2 row-start-1 text-gray-700 text-sm font-bold" for="initials placeholder="XX">Initials:</label>
+              <input class="col-start-2 row-start-2 shadow appearance-none border rounded focus:outline-none focus:shadow-outline placeholder-gray-300"
+                      name="initials" id="initials" type="text" maxlength="2" placeholder="XX">
+              <button type="submit" class="col-start-3 row-start-2  bg-green-300 hover:bg-green-700 text-white font-bold
+                                            rounded focus:outline-none focus:shadow-outline" >
+                Join Game
+              </button>
+            </div>
+          </form>
+          <p class="text-left pt-2 font-bold">Or start a new one?</p>
+          <p class="text-left pb-4">Enter your one or two character initials, select your Work in Progress (WIP) limit type and click "New Game"</p>
+          <form phx-submit="new_game">
+            <div class="grid grid-cols-3 grid-rows-3 gap-2">
+              <label class="col-start-2 row-start-1 text-gray-700 text-sm font-bold" for="initials">Initials:</label>
+              <input class="col-start-2 row-start-2 shadow appearance-none border rounded focus:outline-nonefocus:shadow-outline placeholder-gray-300"
+                    name="initials" id="initials" type="text" maxlength="2" placeholder="XX">
+              <button type="submit" class="col-start-3 row-start-2 bg-green-300 hover:bg-green-700 text-white font-bold
+                                            rounded focus:outline-none focus:shadow-outline" >
+                New Game
+              </button>
+              <div class="col-start-1 row-start-3">
+                <input type="radio" id="none" name="wip" value="none" checked>
+                <label for="none">None</label>
+              </div>
+                <div class="col-start-2 row-start-3">
+                <input type="radio" id="std" name="wip" value="std">
+                <label for="std">WIP</label>
+              </div>
+              <div class="col-start-3 row-start-3">
+                <input type="radio" id="con" name="wip" value="con">
+                <label for="con">ConWIP</label>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     """
   end
 
@@ -338,41 +388,41 @@ defmodule GamesRoomWeb.ChangebanLive do
     <div class="grid grid-cols-cb grid-rows-cb my-3 container border border-gray-800 text-center">
       <%= headers(assigns) %>
       <div class="col-start-1 row-start-3 row-span-5 border border-gray-800 bg-contain"
-           style="background-image: url('images/au_text.svg'); background-repeat: no-repeat">
+           style="background-image: url('/images/au_text.svg'); background-repeat: no-repeat">
         <%= active_items(assigns, 0) %>
       </div>
       <div class="col-start-2 row-start-3 row-span-5 border border-gray-800 bg-contain"
-           style="background-image: url('images/nc_text.svg'); background-repeat: no-repeat">
+           style="background-image: url('/images/nc_text.svg'); background-repeat: no-repeat">
         <%= active_items(assigns, 1) %>
       </div>
       <div class="col-start-3 row-start-3 row-span-5 border border-gray-800 bg-contain"
-           style="background-image: url('images/va_text.svg'); background-repeat: no-repeat">
+           style="background-image: url('/images/va_text.svg'); background-repeat: no-repeat">
         <%= active_items(assigns, 2) %>
       </div>
       <div class="col-start-4 row-start-3 row-span-5 border border-gray-800 bg-contain"
-           style="background-image: url('images/vp_text.svg'); background-repeat: no-repeat">
+           style="background-image: url('/images/vp_text.svg'); background-repeat: no-repeat">
         <%= active_items(assigns, 3) %>
       </div>
 
       <div class="col-start-5 row-start-4 col-span-4 border border-gray-800 bg-contain"
-           style="background-image: url('images/dn_text.svg'); background-repeat: no-repeat; background-position: center;">
+           style="background-image: url('/images/dn_text.svg'); background-repeat: no-repeat; background-position: center;">
         <%= completed_items(assigns, 4) %>
       </div>
 
       <div class="col-start-5 row-start-7 row-span-2 border border-gray-800 bg-contain"
-           style="background-image: url('images/rj_text.svg'); background-repeat: no-repeat">
+           style="background-image: url('/images/rj_text.svg'); background-repeat: no-repeat">
         <%= completed_items(assigns, 5) %>
       </div>
       <div class="col-start-6 row-start-7 row-span-2 border border-gray-800 bg-contain"
-           style="background-image: url('images/rj_text.svg'); background-repeat: no-repeat">
+           style="background-image: url('/images/rj_text.svg'); background-repeat: no-repeat">
         <%= completed_items(assigns, 6) %>
       </div>
       <div class="col-start-7 row-start-7 row-span-2 border border-gray-800 bg-contain"
-           style="background-image: url('images/rj_text.svg'); background-repeat: no-repeat">
+           style="background-image: url('/images/rj_text.svg'); background-repeat: no-repeat">
         <%= completed_items(assigns, 7) %>
       </div>
       <div class="col-start-8 row-start-7 row-span-2 border border-gray-800 bg-contain"
-           style="background-image: url('images/rj_text.svg'); background-repeat: no-repeat">
+           style="background-image: url('/images/rj_text.svg'); background-repeat: no-repeat">
         <%= completed_items(assigns, 8) %>
       </div>
     </div>
@@ -449,55 +499,41 @@ defmodule GamesRoomWeb.ChangebanLive do
 
   def render_help_instructions(assigns) do
     ~L"""
-    <p>You can help someone!  Unblock or move someone else's item</p>
+    <p>You cannot move your own items so you can help someone!</p>
+    <p class="font-black" >Unblock or move someone else's item</p>
     """
   end
 
   def render_reject_instructions(assigns) do
     ~L"""
-    <p>Now you have accepted an item you can reject any item on the board</p>
-    <p>Discuss with the others which one to reject</p>
+    <p>You have accepted an item</p>
+    <p>Now you can reject any item on the board</p>
+    <p class="font-black" >Discuss with the other players and reject an item</p>
     """
+  end
+
+  def can_class(action_list) do
+    if ! Enum.empty?(action_list) do
+      "font-black"
+    else
+      "text-grey-700"
+    end
   end
 
   def render_black_instructions(assigns) do
     ~L"""
-    <%=cond do %>
-      <% ! Enum.empty?(@options.block) -> %>
-        <p>You must block one unblocked item</p>
-      <% ! Enum.empty?(@options.start) -> %>
-        <p>You must start one new item</p>
-    <% end %>
+      <p>You must both:</p>
+      <p class="<%= can_class(@options.block) %>">block one unblocked item</p>
+      <p class="<%= can_class(@options.start) %>">and start one new item</p>
     """
   end
 
-  def render_red_instructions(%{options: options} = assigns) do
-    add_move = fn list ->
-      if not Enum.empty?(options.move),
-        do: ["move one of your unblocked items one column right" | list],
-        else: list
-    end
-
-    add_unbk = fn list ->
-      if not Enum.empty?(options.unblock),
-        do: ["unblock one of your blocked items" | list],
-        else: list
-    end
-
-    add_strt = fn list ->
-      if not Enum.empty?(options.start), do: ["start one new item" | list], else: list
-    end
-
-    words = [] |> add_strt.() |> add_unbk.() |> add_move.()
-
-    text =
-      case Enum.count(words) do
-        1 -> "<p>You must " <> List.first(words) <> "</p>"
-        _ -> "<p>You must either " <> Enum.join(words, "</p><p>or ") <> "</p>"
-      end
-
+  def render_red_instructions(assigns) do
     ~L"""
-      <%= raw text %>
+      <p>You must either:</p>
+      <p class="<%= can_class(@options.move) %>">move one of your unblocked items one column right</p>
+      <p class="<%= can_class(@options.unblock) %>">or unblock one of your blocked items</p>
+      <p class="<%= can_class(@options.start) %>">or start one new item</p>
     """
   end
 
@@ -543,14 +579,14 @@ defmodule GamesRoomWeb.ChangebanLive do
       <div class="w-1/6 flex-grow-0 flex
                   border-2 border-black rounded-md">
         <div class="w-1/4 flex flex-col flex-col-reverse">
-          <img class="p-1 object-contain" src="images/black_spade.svg" alt="black spade">
+          <img class="p-1 object-contain" src="/images/black_spade.svg" alt="black spade">
         </div>
         <div class="w-2/4 flex flex-col justify-center">
           <div class="text-center text-black text-2xl">Turn:</div>
           <div class="text-center text-black text-2xl"><%= to_string(@nr) %></div>
         </div>
         <div class="w-1/4 flex flex-col">
-          <img class="p-1 object-contain" src="images/black_club.svg" alt="black club">
+          <img class="p-1 object-contain" src="/images/black_club.svg" alt="black club">
         </div>
       </div>
     """
@@ -561,14 +597,14 @@ defmodule GamesRoomWeb.ChangebanLive do
       <div class="w-1/6 flex-grow-0 flex
                   border-2 border-red-700 rounded-md">
         <div class="w-1/4 flex flex-col">
-          <img class="p-1 object-contain" src="images/red_diamond.svg" alt="red diamond">
+          <img class="p-1 object-contain" src="/images/red_diamond.svg" alt="red diamond">
         </div>
         <div class="w-2/4 w-2/4 flex flex-col justify-center">
           <div class="text-center text-red-700 text-2xl">Turn:</div>
           <div class="text-center text-red-700 text-2xl"><%= to_string(@nr) %></div>
         </div>
         <div class="w-1/4 flex flex-col flex-col-reverse">
-          <img class="p-1 object-contain" src="images/red_heart.svg" alt="red heart">
+          <img class="p-1 object-contain" src="/images/red_heart.svg" alt="red heart">
         </div>
       </div>
     """
