91ed95acfb585cdeabb14a20021c788dee6c2255
BigTom <BigTom@users.noreply.github.com>
2021-09-01 22:39:50 +0100

    Upgrade to phoenix 16.0_rc0 and liveview 1.6.1

--- apps/games_room/mix.exs
+++ apps/games_room/mix.exs
@@ -37,17 +37,17 @@ defmodule GamesRoom.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.4"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix, "~> 1.6.0-rc.0", override: true},
+      {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_view, "~> 0.16.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.7"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
+      {:phoenix_live_dashboard, "~> 0.5"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 0.5"},
       {:gettext, "~> 0.11"},
       {:changeban, in_umbrella: true},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.1"},
-      {:phoenix_live_view, "~> 0.14.4"},
       {:floki, ">= 0.0.0", only: :test},
       {:contex, "~> 0.3.0"}
     ]
