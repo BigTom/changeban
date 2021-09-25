d638839c794d5559151384903cbf9bd2e6159524
BigTom <BigTom@users.noreply.github.com>
2021-09-25 08:23:15 +0100

    Update phoenix to 1.6.0

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Changeban.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0-rc.0", override: true},
+      {:phoenix, "~> 1.6.0", override: true},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.16.0"},
